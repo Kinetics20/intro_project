5f7677c083a26c03f37f6b68c855fb9ded0fcf79
Kinetics20 <kinetics20@gmail.com>
2024-12-10 04:52:13 -0800

    tuple packing, unpacking samples

--- example_2.py
+++ example_2.py
@@ -20,5 +20,16 @@ a, b, c = (1, 2, 3)
 t5 = (1, 2, 3, 4, 5, 6, 7, 9)
 
 a1, b1, *rest = t5
+
 *rest_1, a2, b2 = t5
-print(rest, rest_1)
+
+a3, *rest_2, b3, c3 = t5
+
+print(rest, rest_1, rest_2)
+
+x = 0
+y = 1
+
+# left side tuple unpacking = right side tuple packing
+# TODO ---- L - tuple unpacking | R - tuple packing
+x, y = y, x
