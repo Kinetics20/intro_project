f4fbe3fe9b00ea7c3a2314be2307dfb50f763f7a
Kinetics20 <kinetics20@gmail.com>
2024-12-11 22:36:33 +0100

    testing verbose with fixture

--- test_bank_account.py
+++ test_bank_account.py
@@ -1,8 +1,25 @@
 from decimal import Decimal
 
+import pytest
+
 from bank_account import BankAccount
 
 
-def test_initial_balance():
-    bank_account = BankAccount('Johny', '')
-    assert bank_account.balance == Decimal('0.0')
+@pytest.fixture(scope='function')
+def empty_account():
+    print('\nFixture\n')
+    return BankAccount('Johny', '')
+
+
+def test_initial_default_balance(empty_account):
+    print('\nSTH balance\n')
+    assert empty_account.balance == Decimal('0.0')
+
+
+def test_deposit(empty_account):
+    print('\nSTH balance___________2\n')
+    empty_account.deposit(Decimal('21.37'))
+    assert empty_account.balance == Decimal('21.37')
+
+
+# def withdraw():
