cd4c47c3eca95dd53c5f370450fe1e8fdb93b2bb
Kinetics20 <kinetics20@gmail.com>
2024-12-12 07:02:32 -0800

    add marker for slow test

--- test_bank_account.py
+++ test_bank_account.py
@@ -22,12 +22,12 @@ def test_initial_balance(empty_account, account_with_balance):
     assert empty_account.balance == Decimal('0.0')
     assert account_with_balance.balance == Decimal('100.00')
 
-
+@pytest.mark.slow
 def test_deposit(empty_account):
     empty_account.deposit(Decimal('21.37'))
     assert empty_account.balance == Decimal('21.37')
 
-@pytest.mark.skipif(sys.platform == 'Win32', reason='Win32 issues')
+@pytest.mark.skipif(sys.platform == 'linux', reason='test skipped on linux os')
 def test_withdraw_insufficient_funds(empty_account):
     with pytest.raises(ValueError, match='Insufficient funds'):
         empty_account.withdraw(Decimal('0.50'))
