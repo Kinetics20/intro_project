f55b95631f1ca6fef81ebb2aec721153e790e721
Kinetics20 <kinetics20@gmail.com>
2024-12-12 07:10:27 -0800

    add marker smoke and  slow for test

--- test_bank_account.py
+++ test_bank_account.py
@@ -18,14 +18,18 @@ def account_with_balance():
     return BankAccount('Johny', '', balance=Decimal('100.00'))
 
 
+@pytest.mark.smoke
 def test_initial_balance(empty_account, account_with_balance):
     assert empty_account.balance == Decimal('0.0')
     assert account_with_balance.balance == Decimal('100.00')
 
+
+@pytest.mark.smoke
 @pytest.mark.slow
 def test_deposit(empty_account):
     empty_account.deposit(Decimal('21.37'))
     assert empty_account.balance == Decimal('21.37')
+
 
 @pytest.mark.skipif(sys.platform == 'linux', reason='test skipped on linux os')
 def test_withdraw_insufficient_funds(empty_account):
